e0d13698d63292ce3bb618bfa0b56546e516e195
AbhiJ2706 <43829700+AbhiJ2706@users.noreply.github.com>
2020-12-25 13:46:52 -0500

    added some test files

---
+++ server.mjs
@@ -0,0 +1,11 @@
+function ProjectDiv(props){
+    return (<h1>doot</h1>);
+  }
+  
+  const element = <ProjectDiv h1="hello" />;
+  ReactDOM.render(
+    element,
+    document.getElementById('root')
+  )
+  
+  console.log("this ran")
