53f17fecea035a0d20457eac239f181a4d9af106
AbhiJ2706 <43829700+AbhiJ2706@users.noreply.github.com>
2021-01-06 17:02:35 -0500

    fixed tic-tac-toe glitch

--- res/tictactoe/tictactoe.mjs
+++ res/tictactoe/tictactoe.mjs
@@ -95,6 +95,11 @@ class Game extends React.Component {
         console.log(oPos)
 
         squares[i] = "X";
+
+        if (calculateWinner(squares)) {
+            return;
+        }
+
         squares[oPos] = "O";
 
         this.board[oPos] = -1;
@@ -135,21 +140,21 @@ class Game extends React.Component {
 ReactDOM.render(e(Game, {}, null), document.getElementById("root"));
 
 function calculateWinner(squares) {
-const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-];
-for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-    return squares[a];
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
     }
-}
-return null;
+    return null;
 }
