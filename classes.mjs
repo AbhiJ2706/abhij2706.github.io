7ea74524779fdff43ad60a089630b52db51efab6
AbhiJ2706 <43829700+AbhiJ2706@users.noreply.github.com>
2021-04-27 12:16:41 -0400

    fixed style

--- classes.mjs
+++ classes.mjs
@@ -23,14 +23,34 @@ class ProjectTextBtnContainer extends React.Component {
     render (){
         return e(
             'div',
-            {style: {height: "50%", width: "100%"}},
-            e("div", {style: {height: "70%", width: "100%"}},
+            {className: "plzwork", style: {height: "50%", width: "95%", margin:"2.5%", float: "left", backgroundColor: "#e5e5e5", borderRadius: "5%"}},
+            e("div", {style: {height: "50%", width: "100%"}},
                 e('h1', {style: {fontSize: "5vm", marginLeft: "10%", width: "80%", textAlign: 'center'}}, this.props.headerText),
                 e('p', {style: {marginLeft: "20%", marginRight: "20%", textAlign: 'center', width: '60%'}}, this.props.pText)
             ), 
-            e('div', {style: {display: "flex", alignItems: "center", height: '30%', width: "100%", paddingBottom: "5%"}}, 
-                         e('button', {className: "ui " + this.props.b1Color + " button", onClick: () => {window.location.href = this.props.link1}, style: {position: "relative", height: "75%", width: '47%', margin: "auto"}}, this.props.b1Text),
+            e('div', {style: {
+                                height: '50%', 
+                                width: "50%", 
+                                paddingBottom: "5%", 
+                                marginLeft: "25%", 
+                                marginRight: "25%"
+                            }}, 
+                         e('button', {className: "ui " + this.props.b1Color + " button", 
+                                      onClick: () => {window.location.href = this.props.link1}, 
+                                      style: {position: "relative", 
+                                              height: "50%", 
+                                              width: '100%', 
+                                              marginLeft:"0", 
+                                              marginRight:"0", 
+                                              marginTop: (this.props.numBtn == 1)? "15%" : "0", 
+                                              marginBottom: (this.props.numBtn == 1)? "35%" : "0", 
+                                            }}, this.props.b1Text),
                          (this.props.numBtn != 1)?
-                         e('button', {className: "ui " + this.props.b2Color + " button", onClick: () => {window.location.href = this.props.link2}, style: {height: "75%", width: '47%', margin: "auto", float: "right"}}, this.props.b2Text) : null))
+                         e('button', {className: "ui " + this.props.b2Color + " button", 
+                                      onClick: () => {window.location.href = this.props.link2}, 
+                                      style: {height: "50%", 
+                                              width: '100%', 
+                                              margin: "auto"
+                                            }}, this.props.b2Text) : null))
     }
 }
